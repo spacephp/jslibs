51fefce9341d01f72a89b48a50fd13aac5d1a084
oangia <hqnhatdn@gmail.com>
2021-09-10 11:15:19 +0700

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,64 @@
+var debug = _get("debug", false);
+
+if (debug != false)
+{
+  //console.everything = [];
+  console.defaultLog = console.log.bind(console);
+  console.log = function(){
+    debugMess({"type":"log", "datetime":Date().toLocaleString(), "value":Array.from(arguments)}); 
+    //console.everything.push({"type":"log", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+    console.defaultLog.apply(console, arguments);
+  }
+  console.defaultError = console.error.bind(console);
+  console.error = function(){
+    debugMess({"type":"error", "datetime":Date().toLocaleString(), "value":Array.from(arguments)}); 
+    //console.everything.push({"type":"error", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+    console.defaultError.apply(console, arguments);
+  }
+  console.defaultWarn = console.warn.bind(console);
+  console.warn = function(){
+    debugMess({"type":"warn", "datetime":Date().toLocaleString(), "value":Array.from(arguments)}); 
+    //console.everything.push({"type":"warn", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+    console.defaultWarn.apply(console, arguments);
+  }
+  console.defaultDebug = console.debug.bind(console);
+  console.debug = function(){
+    debugMess({"type":"debug", "datetime":Date().toLocaleString(), "value":Array.from(arguments)}); 
+    //console.everything.push({"type":"debug", "datetime":Date().toLocaleString(), "value":Array.from(arguments)});
+    console.defaultDebug.apply(console, arguments);
+  }
+
+  $("body").ready(function () {
+    $("body").append("<code id=\"debug\"></code>");
+  })
+}
+
+function debugMess(mes) {
+  if (typeof mes == 'object' ) {
+    mes = JSON.stringify(mes);
+  }
+  if ($("#debug").length) {
+    $("#debug").append("<br/><hr/><br/>");
+    $("#debug").append(mes + "<br />");
+  } else {
+    alert(mes);
+  }
+}
+
+function _get(parameterName, def = "") {
+  var result = null,
+      tmp = [];
+  var items = location.search.substr(1).split("&");
+  for (var index = 0; index < items.length; index++) {
+      tmp = items[index].split("=");
+      if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+  }
+  if (result == undefined) {
+    return def;
+  }
+  return result;
+}
+
 function dateToString(date) {
   let d = date.getDate();
   let m = date.getMonth() + 1;
