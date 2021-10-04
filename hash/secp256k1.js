9e5b6e89aa3a4c0106a97689f527574277cc41ab
oangia <hqnhatdn@gmail.com>
2021-10-04 13:27:55 +0700

    Update secp256k1.js

--- hash/secp256k1.js
+++ hash/secp256k1.js
@@ -6,4 +6,17 @@ class Secp256k1 {
     let Gy = 32670510020758816978083085130507043184471273380659243275938904335757337482424;
     this.GPoint = [Gx,Gy];
   }
+
+  EccMultiply(ScalarHex) {
+    if (ScalarHex < 1000000000000 or ScalarHex >= this.N) {
+      return false;
+    }
+    let ScalarBin = str(bin(ScalarHex))[2:]
+    let Q = this.GPoint
+    for i in range (1, len(ScalarBin)): # This is invented EC multiplication.
+        Q=ECdouble(Q); # print "DUB", Q[0]; print
+        if ScalarBin[i] == "1":
+            Q=ECadd(Q,GenPoint); # print "ADD", Q[0]; print
+    return (Q)
+  }
 }
