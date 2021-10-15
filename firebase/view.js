98129f57fcfe2c233dd9e3a51f717f5343f18790
oangia <hqnhatdn@gmail.com>
2021-10-15 17:11:07 +0700

    Update view.js

--- firebase/view.js
+++ firebase/view.js
@@ -4,17 +4,35 @@ class View {
   }
 
   list(data) {
+    let config = {
+      fullname: {
+        header: "Full Name
+      },
+      equity: {
+        header: "Equity"
+      },
+      ccq: {
+        header: "CCQ"
+      },
+      created_at: {
+        header: "Created At"
+      }
+    }
     let html = '<table class="table m-0">';
     html += '<thead>';
     html += '<tr>';
-    html += '<th>Order ID</th>';
+    config.forEach(item => {
+      html += '<th>' + item.header + '</th>';
+    });
     html += '</tr>';
     html += '</thead>';
     html += '<tbody>';
     data.forEach(item => {
       let itemData = item.data();
       html += '<tr id="' + item.id + '">';
-      html += '<td>Item</td>';                 
+      config.forEach((configItem, field) => {
+        html += '<td>' + itemData[field] +'</td>';    
+      });
       html += '</tr>';
     });
     html += '</tbody>';
