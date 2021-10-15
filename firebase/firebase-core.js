2e03151f89464b4dc18596bad528d8edc0d49ab2
oangia <hqnhatdn@gmail.com>
2021-10-15 15:38:04 +0700

    Build

--- firebase/firebase-core.js
+++ firebase/firebase-core.js
@@ -81,40 +81,46 @@ const logout = document.querySelector('#logout');
 const signupForm = document.querySelector('#signup-form');
 
 // login
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  // get user info
-  const email = loginForm['login-email'].value;
-  const password = loginForm['login-password'].value;
-  // log the user in
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    // close the signup modal & reset form
-    $("#modal-login").modal("hide");
-    loginForm.reset();
+if (loginForm != undefined) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+    // log the user in
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+      // close the signup modal & reset form
+      $("#modal-login").modal("hide");
+      loginForm.reset();
+    });
   });
-});
+}
 
 // logout
-logout.addEventListener('click', (e) => {
-  e.preventDefault();
-  auth.signOut();
-});
+if (logout != undefined) {
+  logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+  });
+}
 
 // signup
-signupForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  // get user info
-  const email = signupForm['signup-email'].value;
-  const password = signupForm['signup-password'].value;
-  // sign up the user & add firestore data
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    return db.collection('users').doc(cred.user.uid).set(additionalData());
-  }).then(() => {
-    // close the signup modal & reset form
-    $("#modal-signup").modal("hide");
-    signupForm.reset();
+if (signupForm != undefined) {
+  signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+    // sign up the user & add firestore data
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+      return db.collection('users').doc(cred.user.uid).set(additionalData());
+    }).then(() => {
+      // close the signup modal & reset form
+      $("#modal-signup").modal("hide");
+      signupForm.reset();
+    });
   });
-});
+}
 
 // listen for auth status changes
 auth.onAuthStateChanged(async user => {
