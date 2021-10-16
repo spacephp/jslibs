f4df4366280c65a3691a47c48f9183087b9b8a84
oangia <hqnhatdn@gmail.com>
2021-10-16 10:38:43 +0700

    Update firebase-core.js

--- firebase/firebase-core.js
+++ firebase/firebase-core.js
@@ -108,6 +108,7 @@ $( document ).ready(function() {
       const password = signupForm['signup-password'].value;
       // sign up the user & add firestore data
       auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        if (additionalData == undefined) return;
         return db.collection('users').doc(cred.user.uid).set(additionalData());
       }).then(() => {
         // close the signup modal & reset form
@@ -120,11 +121,8 @@ $( document ).ready(function() {
   // listen for auth status changes
   auth.onAuthStateChanged(async user => {
     if (!user) {
-      if (loggedOut != undefined) {
-        loggedOut();
-      } else {
-        location.reload();
-      }
+      if (loggedOut != undefined) location.reload();
+      loggedOut();
     } else {
       loggedIn(user);
     }
