ae3550df858b1025d3aafab4b2d307083fca7ac1
oangia <hqnhatdn@gmail.com>
2021-10-16 13:42:47 +0700

    Update firebase-core.js

--- firebase/firebase-core.js
+++ firebase/firebase-core.js
@@ -148,20 +148,19 @@ class View {
 
   static async list(crud) {
     let ref = new Model(crud.collection);
-    let config = crud.config;
     
-    let data = await ref.orderBy(config.orderByField, config.orderByType)
-             .startAfter(config.lastedDoc || new Date())
-             .limit(config.pagination)
+    let data = await ref.orderBy(crud.orderByField, crud.orderByType)
+             .startAfter(crud.lastedDoc || new Date())
+             .limit(crud.pagination)
              .get();
 
-    config.lastDoc = data[data.length - 1];
+    crud.lastDoc = data[data.length - 1];
 
     let html = '<table class="table m-0">';
     html += '<thead>';
     html += '<tr>';
-    config.table.forEach(item => {
-      html += '<th>' + item.header + '</th>';
+    crud.list.forEach(item => {
+      html += '<th>' + item.config.header + '</th>';
     });
     html += '</tr>';
     html += '</thead>';
@@ -169,7 +168,7 @@ class View {
     data.forEach(item => {
       let itemData = item.data();
       html += '<tr id="' + item.id + '">';
-      config.table.forEach(configItem => {
+      crud.list.forEach(configItem => {
         html += '<td>' + itemData[configItem.field] +'</td>';
       });
       html += '</tr>';
