36216478da1f1c1e3c5a95d8904b55ff279f4c3f
oangia <hqnhatdn@gmail.com>
2021-10-16 13:27:02 +0700

    Update firebase-core.js

--- firebase/firebase-core.js
+++ firebase/firebase-core.js
@@ -185,3 +185,30 @@ class View {
   }
 }
 
+class Crud {
+  constructor(collection) {
+    this.collection = collection;
+    this.orderBy = "created_at";
+    this.orderByType = "desc";
+    this.lastedDoc = null;
+    this.list = [];
+    this.pagination = 10;
+  }
+
+  orderBy(field, type="desc") {
+    this.orderBy = field;
+    this.orderByType = type;
+  }
+  
+  paginate(pagination) {
+    this.pagination = pagination;
+  }
+  
+  addList(field, config = {}) {
+    this.list.push({field: field, config: config});
+  }
+
+  async list() {
+    return await View.list(this);
+  }
+}
