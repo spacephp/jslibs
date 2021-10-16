ce2742645aa844ca576d5dc65ad50cff6d9331ae
oangia <hqnhatdn@gmail.com>
2021-10-16 11:23:51 +0700

    Create crud.js

---
+++ firebase/crud.js
@@ -0,0 +1,17 @@
+class Crud {
+  constructor() {
+    this.orderBy = "created_at";
+    this.orderByType = "desc
+    this.lastDoc = null;
+    this.list = [];
+    this.pagination = 10;
+  }
+  
+  paginate(pagination) {
+    this.pagination = pagination;
+  }
+  
+  addListConfig(field, config = {}) {
+    this.list.push({field: field, config: config});
+  }
+}
